adac56590cb80b1496d1204a866a368aa528e254
Mikayel5464 <125598703+Mikayel5464@users.noreply.github.com>
2026-06-30 19:06:55 +0400

    Initial File Commit
    Placing Unreal project into Github

---
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float MemberFloat = 10.0f;
+
+	UPROPERTY(VisibleAnywhere)
+	int MemberInt = 2;
+
+	UPROPERTY(EditAnywhere)
+	FVector TestVector = FVector(11.2f, 3.4f, 7.6f);
+
+};
